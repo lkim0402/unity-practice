a6ae2d0da69a2f595892dec07f4a894adc395ccf
Leejun <estherleejunkim@gmail.com>
2024-08-03 22:44:00 +0900

    Initial commit!

---
+++ Assets/Mover.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    float xValue = 0;
+    float yValue = 0.01f;
+    float zValue = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(xValue, yValue, zValue);
+        
+    }
+}
